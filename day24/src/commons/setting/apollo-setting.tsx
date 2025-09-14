0485c114638829e2e50746bdb397a03a9f1cd2a8
hanck1324-ship-it <hanck1324@gmail.com>
2025-09-15 06:40:55 +0900

    plz

--- day24/src/commons/setting/apollo-setting.tsx
+++ day24/src/commons/setting/apollo-setting.tsx
@@ -1,12 +1,20 @@
+"use client"
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+interface IApolloSetting {
+    children: React.ReactNode
+}
 export default function ApolloSetting(props: IApolloSetting) {
     const client = new ApolloClient({
+        // 아래 uri 주소를 올바른 주소로 수정해주세요!
         uri: "http://main-practice.codebootcamp.co.kr/graphql",
         cache: new InMemoryCache()
     })    
 
     return (
         <ApolloProvider client={client}>
-            {props.children}  {/* 여기서 children을 렌더링 */}
+            {props.children}
         </ApolloProvider>
     )
 }
