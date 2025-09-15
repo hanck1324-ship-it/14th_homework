796ce7a6a98cceef7ebec8b6c68f3d002c75ac55
hanck1324-ship-it <hanck1324@gmail.com>
2025-09-15 09:22:21 +0900

    whyrano

--- day23/src/commons/settings/apollo-setting.tsx
+++ day23/src/commons/settings/apollo-setting.tsx
@@ -1,19 +1,18 @@
 "use client"
 
-// 1. HttpLink를 추가로 import 합니다.
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
 
 interface IApolloSetting {
     children: React.ReactNode
 }
+
 export default function ApolloSetting(props: IApolloSetting) {
-    // 2. HttpLink를 먼저 만들어줍니다.
     const httpLink = createHttpLink({
+        // ❗️ 바로 이 주소가 올바른 서버 주소입니다.
         uri: "http://main-practice.codebootcamp.co.kr/graphql",
     });
 
     const client = new ApolloClient({
-        // 3. uri 대신 link 옵션을 사용해서 연결합니다.
         link: httpLink,
         cache: new InMemoryCache()
     });
